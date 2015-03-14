89302ec0bd6cd8ea5542ff13b562a7e8a3a6e4a0
Eric Heikes <eheikes@gmail.com>
2015-03-13 22:35:02 -0500

    License under Apache 2.0.

--- app/ShoppingListModule.js
+++ app/ShoppingListModule.js
@@ -1,3 +1,8 @@
+/*!
+ * Shopping List
+ * http://github.com/eheikes/shopping-list
+ * License: Apache-2.0
+ */
 angular.module('shoppingList', ['ngSanitize', 'ui.select', 'ng.group'])
   .config(function(uiSelectConfig) {
     'use strict';
