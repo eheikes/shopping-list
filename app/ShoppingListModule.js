a22470db63eadf12d68552f8ac8114ed6fb8b2c3
Eric Heikes <eheikes@gmail.com>
2015-05-14 19:45:59 -0500

    Include copyright notice in JS comments.

--- app/ShoppingListModule.js
+++ app/ShoppingListModule.js
@@ -1,6 +1,7 @@
 /*!
  * Shopping List
  * http://github.com/eheikes/shopping-list
+ * Copyright (c) 2015 Eric Heikes
  * License: Apache-2.0
  */
 angular.module('shoppingList', ['ngSanitize', 'ui.select', 'ng.group']).config(
